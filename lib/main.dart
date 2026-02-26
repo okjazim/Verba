8109b713009a0ddd686081f3e65952e056ef6ff9
okjazim <jazimjunaith02@gmail.com>
2026-02-26 15:47:25 +0100

    initialized main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'core/theme/app_theme.dart';
+import 'features/home/home_screen.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const VerbaApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class VerbaApp extends StatelessWidget {
+  const VerbaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Verba',
+      theme: AppTheme.light,
+      home: const HomeScreen(),
     );
   }
 }
