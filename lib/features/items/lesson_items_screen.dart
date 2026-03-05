4d57aa6cfa81371d4ec0dba25045717b977567f9
okjazim <jazimjunaith02@gmail.com>
2026-03-05 19:08:56 +0100

    load lesson items

--- lib/features/items/lesson_items_screen.dart
+++ lib/features/items/lesson_items_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-class LessonItemsScreen extends StatelessWidget {
+import 'package:verba/core/data/lesson_item_repository.dart';
+import 'package:verba/core/models/lesson_item.dart';
+
+class LessonItemsScreen extends StatefulWidget {
   final String lessonId;
   final String lessonTitle;
 
@@ -11,14 +14,55 @@ class LessonItemsScreen extends StatelessWidget {
   });
 
   @override
+  State<LessonItemsScreen> createState() => _LessonItemsScreenState();
+}
+
+class _LessonItemsScreenState extends State<LessonItemsScreen> {
+  final LessonItemRepository _repository = InMemoryLessonItemRepository();
+  List<LessonItem> _items = const [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  Future<void> _loadItems() async {
+    final items = await _repository.getByLessonId(widget.lessonId);
+    setState(() {
+      _items = items;
+      _isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('$lessonTitle items'),
+        title: Text('${widget.lessonTitle} items'),
       ),
-      body: const Center(
-        child: Text('Lesson items will appear here.'),
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : _items.isEmpty
+              ? const Center(
+                  child: Text('No items yet. We will add some next.'),
+                )
+              : ListView.separated(
+                  itemCount: _items.length,
+                  separatorBuilder: (_, __) => const Divider(height: 1),
+                  itemBuilder: (context, index) {
+                    final item = _items[index];
+                    return ListTile(
+                      title: Text(item.front),
+                      subtitle: Text(item.back),
+                      trailing: Text(
+                        item.type == LessonItemType.word ? 'word' : 'phrase',
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                    );
+                  },
+                ),
     );
   }
 }
